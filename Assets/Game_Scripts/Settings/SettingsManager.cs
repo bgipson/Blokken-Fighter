d37181c241d943a9978ad56f20a5b7a5d7d669ab
Kevin Tang <kev123361@gmail.com>
2018-02-09 13:40:21 -0500

    Audio Options. Still Buggy. Maybe b/c of controller input?

---
+++ Assets/Game_Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SettingsManager : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioSource BGM;
+    public GameSettings gameSettings;
+    public Button applyButton;
+    
+
+    private void OnEnable()
+    {
+        gameSettings = new GameSettings();
+        volumeSlider.onValueChanged.AddListener(delegate { OnVolumeChange(); });
+
+        applyButton.onClick.AddListener(delegate { OnApplyButtonClicked(); });
+    }
+
+    public void OnVolumeChange()
+    {
+        BGM.volume = volumeSlider.value = gameSettings.volume;
+    }
+
+    public void OnApplyButtonClicked()
+    {
+        SaveSettings();
+    }
+
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameSettings, true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+
+    public void LoadSettings()
+    {
+
+    }
+}
