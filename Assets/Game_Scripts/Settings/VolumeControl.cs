699df045f41c7c0d95c0c8cdfcba8089ba7a0a51
Kevin Tang <kev123361@gmail.com>
2018-03-16 14:20:54 -0400

    Initial Audio Option

---
+++ Assets/Game_Scripts/Settings/VolumeControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VolumeControl : MonoBehaviour {
+
+    float minValue = 0f, maxValue = 1f, variationAmount = .1f;
+    Slider sliderRef;
+
+    void Start()
+    {
+        sliderRef = this.gameObject.GetComponent<Slider>();
+        sliderRef.minValue = minValue;
+        sliderRef.maxValue = maxValue;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+
+            sliderRef.value -= variationAmount;
+        }
+        else if (Input.GetKeyDown("x"))
+        {
+            sliderRef.value += variationAmount;
+        }
+    }
+}
